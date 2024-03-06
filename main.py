3aa8ee91e32284bac006f15ac43fb7fb4c7594c1
NIkita <nikkuzya21@gmail.com>
2024-03-06 14:53:29 +0300

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from config_reader import config
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def main():
+    bot = Bot(token=config.bot_token.get_secret_value(), parse_mode='HTML')
+    dp = Dispatcher()
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
